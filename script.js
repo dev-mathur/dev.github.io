98dda034c0160094c5ec989a5ce911c0c5b9e475
Dev Mathur <mathurdev@outlook.com>
2019-03-22 21:26:36 -0700

    added more js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// Changes navbar color
 $(window).scroll(function(){
 	$('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
 });
+
+$(document).ready(function(){
+	$("p").mouseover(function(){
+	  $("p").css("color", "red");
+	});
+	$("p").mouseout(function(){
+	  $("p").css("color", "black");
+	});
+  });
+
+  $(document).ready(function(){
+	$("h1").mouseover(function(){
+	  $("h1").css("color", "green");
+	});
+	$("h1").mouseout(function(){
+	  $("h1").css("color", "black");
+	});
+  });
